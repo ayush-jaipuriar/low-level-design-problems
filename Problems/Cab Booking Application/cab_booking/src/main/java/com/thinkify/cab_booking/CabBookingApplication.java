3a0b6271a0c7c665c1a602e57508a50e3a004472
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-08 16:38:08 +0530

    Add Tests to CommandLineRunner in CabBookingApplication

--- Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/CabBookingApplication.java
+++ Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/CabBookingApplication.java
@@ -1,7 +1,20 @@
 package com.thinkify.cab_booking;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.thinkify.cab_booking.controllers.DriverController;
+import com.thinkify.cab_booking.controllers.RideController;
+import com.thinkify.cab_booking.controllers.UserController;
+import com.thinkify.cab_booking.models.Driver;
+import com.thinkify.cab_booking.models.Ride;
+import com.thinkify.cab_booking.repository.DriverRepository;
+import com.thinkify.cab_booking.repository.UserRepository;
+import com.thinkify.cab_booking.services.RideService;
 
 @SpringBootApplication
 public class CabBookingApplication {
@@ -11,5 +24,38 @@ public class CabBookingApplication {
 		Test test = new Test();
 		test.runDemo();		
 	}
+
+	@Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository, DriverRepository driverRepository, RideService rideService) {
+		return args -> {
+            UserController userController = new UserController(userRepository);
+            DriverController driverController = new DriverController(driverRepository);
+            RideController rideController = new RideController(rideService);
+
+            // Onboard users
+            userController.addUser("Abhishek, M, 23");
+            userController.addUser("Rahul, M, 29");
+            userController.addUser("Nandini, F, 22");
+
+			// Add Drivers
+			driverController.addDriver("Driver1, M, 22", "Swift:KA-01-12345", "(10,1)");
+			driverController.addDriver("Driver2, M, 29", "Swift:KA-01-12345", "(11,10)");
+			driverController.addDriver("Driver3, M, 24", "Swift:KA-01-12345", "(5,3)");
+
+            // Find rides
+            List<Driver> availableDrivers = rideController.findRide("Abhishek", "(0,0)", "(20,1)");
+            System.out.println(availableDrivers.isEmpty() ? "No ride found" : availableDrivers);
+
+            availableDrivers = rideController.findRide("Rahul", "(10,0)", "(15,3)");
+            System.out.println(availableDrivers.isEmpty() ? "No ride found" : availableDrivers);
+
+            availableDrivers = rideController.findRide("Nandini", "(15,6)", "(20,4)");
+            System.out.println(availableDrivers.isEmpty() ? "No ride found" : availableDrivers);
+
+            // Choose ride
+            Ride ride = rideController.chooseRide("Rahul", "Driver1", "(10,0)", "(15,3)");
+            System.out.println("Ride chosen: " + ride);
+        };
+    }
 
 }
