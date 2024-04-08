4dc5988285cf3027b199b1b86a6c04b3ed5787cd
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-08 16:06:05 +0530

    Refactor DriverController

--- Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/controllers/DriverController.java
+++ Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/controllers/DriverController.java
@@ -18,28 +18,23 @@ public class DriverController {
     }
 
     public void addDriver(String driverDetails, String vehicleDetails, String currentLocation) {
-        // Parse driverDetails string to extract driver information
         String[] driverInfo = driverDetails.split(",");
         String name = driverInfo[0].trim();
         String gender = driverInfo[1].trim();
         int age = Integer.parseInt(driverInfo[2].trim());
 
-        // Parse vehicleDetails string to extract vehicle information
-        String[] vehicleInfo = vehicleDetails.split(",");
+        String[] vehicleInfo = driverInfo[3].split(":");
         String model = vehicleInfo[0].trim();
         String licenseNumber = vehicleInfo[1].trim();
 
-        // Parse currentLocation string to extract location information
-        String[] locationInfo = currentLocation.replaceAll("[()]", "").split(",");
+        String[] locationInfo = driverInfo[4].replaceAll("[()]", "").split(",");
         int x = Integer.parseInt(locationInfo[0].trim());
         int y = Integer.parseInt(locationInfo[1].trim());
 
-        // Create new instances of Vehicle, Location, and Driver
         Vehicle vehicle = new Vehicle(model, licenseNumber);
         Location currentLocationObj = new Location(x, y);
         Driver driver = new Driver(name, gender, age, vehicle, currentLocationObj);
 
-        // Add the driver to the repository
         driverRepository.addDriver(driver);
     }
 }
