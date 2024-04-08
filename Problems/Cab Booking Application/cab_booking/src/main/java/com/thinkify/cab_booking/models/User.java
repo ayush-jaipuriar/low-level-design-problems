d9697d7cd78e0ece8a3f42510f608937a34156a4
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-08 14:08:41 +0530

    LLD : Cab Booking Problem, initial commit

---
+++ Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/models/User.java
@@ -0,0 +1,44 @@
+package com.thinkify.cab_booking.models;
+
+public class User {
+    private String name;
+    private String gender;
+    private int age;
+    
+    public User(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", gender=" + gender + ", age=" + age + "]";
+    }
+
+    
+}
---
+++ Problems/Cab Booking Application/cab_booking/src/test/java/com/thinkify/cab_booking/CabBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkify.cab_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
