30db848de1ae767832fab2c6df64e753d8c69179
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-08 14:10:01 +0530

    Add Location Model

---
+++ Problems/Cab Booking Application/cab_booking/src/main/java/com/thinkify/cab_booking/models/Location.java
@@ -0,0 +1,36 @@
+package com.thinkify.cab_booking.models;
+
+public class Location {
+    private int x;
+    private int y;
+
+    public Location() {
+    }
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Location [x=" + x + ", y=" + y + "]";
+    }
+
+}
